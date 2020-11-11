03d4566009193fdd918090f55042f5360f18ecfb
CStank89 <C.Stank89@gmail.com>
2020-11-11 14:36:41 -0500

    started fleshing out the enhancer functions

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,11 +6,20 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
-}
+  return {
+    ...item,
+    enhancement:
+      item.enhancement < 20 ? item.enhancement + 1 : item.enhancement,
+  };
 
 function fail(item) {
-  return { ...item };
+  const enhance = item.enhancement
+  const durable = item.durability
+  return{
+    ...item,
+    durability: enhace < 15 ? durable - 5 : durable - 10,
+    enhancement: enhance > 16 ? enhance -1 : enhance
+  }
 }
 
 function repair(item) {
