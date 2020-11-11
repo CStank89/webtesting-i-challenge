49c8ba32098734be902c6d420c4dbcda9dd88e25
CStank89 <C.Stank89@gmail.com>
2020-11-11 14:40:50 -0500

    finished the enhancer functions on to the testing

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -11,21 +11,31 @@ function success(item) {
     enhancement:
       item.enhancement < 20 ? item.enhancement + 1 : item.enhancement,
   };
+}
 
 function fail(item) {
-  const enhance = item.enhancement
-  const durable = item.durability
-  return{
+  const enhance = item.enhancement;
+  const durable = item.durability;
+  return {
     ...item,
     durability: enhace < 15 ? durable - 5 : durable - 10,
-    enhancement: enhance > 16 ? enhance -1 : enhance
-  }
+    enhancement: enhance > 16 ? enhance - 1 : enhance,
+  };
 }
 
 function repair(item) {
-  return { ...item };
+  return {
+    ...item,
+    duarablity: 100,
+  };
 }
 
 function get(item) {
-  return { ...item };
+  const enhance = item.enhancement;
+  const itemName = item.name;
+
+  return {
+    ...item,
+    name: enhance > 0 ? `[+${enhance}]${itemName}` : `${itemName}`,
+  };
 }
